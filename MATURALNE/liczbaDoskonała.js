5a749831db9c49a8b4c3bdb376318949049c3e31
sandrakorcz <sandra.sisi.official@gmail.com>
2019-09-05 19:50:20 +0200

    liczba doskonała

--- MATURALNE/liczbaDoskonała.js
+++ MATURALNE/liczbaDoskonała.js
@@ -1,1 +1,9 @@
-Wska¿ czy liczba jest doskona³a, czyli jej dzielniki po jej zsumowaniu tworz¹ j¹ sam¹.
+function zadanie3 (n) {
+    var suma = 0; //do zsumowania dzielników
+    var x;      //dzielniki
+    for (x=1,x<n,x++)
+        if (n%x==0); //wyznaczanie dzielników
+            suma=suma+x; //dodawanie dzielników
+            suma==n; //sprawdzanie czy suma jest równa liczbie n
+}
+    return true; //zwraca true je¿eli powy¿sze warunki siê zgadzaj±
