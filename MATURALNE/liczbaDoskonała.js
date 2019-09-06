67e487fe5e868fcbd9bd83f954d6c84bc97869c5
sandrakorcz <sandra.sisi.official@gmail.com>
2019-09-06 11:40:38 +0200

    kk

--- MATURALNE/liczbaDoskonała.js
+++ MATURALNE/liczbaDoskonała.js
@@ -1,7 +1,7 @@
 function zadanie3 (n) {
     var suma = 0; //do zsumowania dzielników
     var x;      //dzielniki
-    for (x=1,x<n,x++) //czego tu brakuje
+    for (x=1,x<n,x++)
         if (n%x==0); //wyznaczanie dzielników
             suma=suma+x; //dodawanie dzielników
             suma==n; //sprawdzanie czy suma jest równa liczbie n
