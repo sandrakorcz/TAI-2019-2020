fcd8a1599901d7f6c8f05560841506dd6ef7076c
sandrakorcz <sandra.sisi.official@gmail.com>
2019-09-15 12:21:00 +0200

    zrobić skrypty, css itp

---
+++ E.14-arkusze/e14-04-18.06/kulinaria.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Restauracja Kulinaria.pl</title>
+    <link rel="stylesheet" type="text/css" href="styl4.css">
+</head>
+<body>
+    <div id="baner">
+        <h2>Restauracja Kulinaria.pl Za[rasza!</h2>
+    </div>
+    <div id="panel_lewy">
+       <p>Dania mięsne zamówisz już od <php>
+
+       </php> złotych</p>
+       <br>
+       <img src="menu.jpg" alt="Pyszne spaghetti">
+       <a href="menu.jpg">Pobierz obraz</a>
+    </div>
+    <div id="panel_srodkowy">
+        <h3>Przekąski</h3>
+        <php?>
+        </php?>
+    </div>
+<div id="panel_prawy">
+        <h3>Napoje</h3>
+            <php?>
+            </php?>
+    </div>
+    <footer>
+        Stronę internetową opracowali: <i>Sandra Korcz i Szymon Modelski</i>
+    </footer>
+</body>
+</html>
